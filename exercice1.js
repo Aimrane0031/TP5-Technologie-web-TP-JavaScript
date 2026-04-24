89391c97923c85aa5d2a6baae35128ca570b04c0
Aimrane0031 <imranmarghine825@gmail.com>
2026-04-25 00:19:39 +0200

    Create exercice1.js

---
+++ exercice1.js
@@ -0,0 +1,10 @@
+let a = prompt("donner premier nombre")
+let b = prompt("donner deuxieme nombre")
+
+a = Number(a)
+b = Number(b)
+
+console.log("la somme = " + (a+b))
+console.log("la difference = " + (a-b))
+console.log("la multiplication = " + (a*b))
+console.log("la division = " + (a/b))
